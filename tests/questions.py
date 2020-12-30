80dd1008782f094661a5c88c2f76fb934593f8b9
et-sollertis-animi <hiaarushgupta@gmail.com>
2020-12-29 19:42:05 -0800

    updated questions test to match the change

--- tests/questions.py
+++ tests/questions.py
@@ -5,7 +5,7 @@ import json
 online = "https://ldkfjaslditqwopri032-ruiopfc39mircrmi023.asdfasdirpwea09.repl.co/questions"
 local = "http://127.0.0.1:5000/questions"
 
-post = {"data" : [0, 0, 10, 0, "[0]", str(datetime.datetime.now().strftime(r"%m/%d/%Y")), "Is this really cool!"]}
+post = {"data" : [0, 0, 10, 0, "[0]" , "[0]", str(datetime.datetime.now().strftime(r"%m/%d/%Y")), "Is this really cool!"]}
 put = {"id" : 0, "column" : "upvotes", "new" : 11}
 delete = {"id" : 0}
 
